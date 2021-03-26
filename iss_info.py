12b27aaa056b92b7a7c9aad783c29de629d2f5b8
shaumikkhanna <shaumikkhanna@gmail.com>
2021-03-26 14:31:46 +0530

    Initial commit

---
+++ iss_info.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import datetime
+
+
+def get_data(lat=None, lon=None):
+	"""
+	Gets the relevant data using coord from the iss api and converts it to a dict
+	"""
+	if lat is None or lon is None:
+		coord = (28.6691,77.0929)
+	else:
+		coord = (lat, lon)
+
+	URL1 = 'http://api.open-notify.org/iss-now.json'
+	r = requests.get(URL1)
+	positional_data = json.loads(r.text)
+
+	URL2 = 'http://api.open-notify.org/iss-pass.json'
+	payload = {'lat': float(coord[0]), 'lon': float(coord[1])}
+	r = requests.get(URL2, params=payload)
+	passes_data = json.loads(r.text)
+
+	URL3 = 'http://api.open-notify.org/astros.json'
+	r = requests.get(URL3)
+	crew_data = json.loads(r.text)
+
+	return positional_data, passes_data, crew_data
+
+
+def display_data(): 
+	positional_data, passes_data, crew_data = get_data()
+
+	long_now = float(positional_data['iss_position']['longitude'])
+	long_now = str(abs(long_now)) + ' Degrees {}'.format('East' if long_now > 0 else 'West')
+	lat_now = float(positional_data['iss_position']['latitude'])
+	lat_now = str(abs(lat_now)) + ' Degrees {}'.format('North' if lat_now > 0 else 'South')
+
+	passes = [
+		(response['duration'], 
+			datetime.datetime.fromtimestamp(response['risetime']).strftime('%d %b %H:%M:%S'))\
+		for response in passes_data['response']
+	]
+
+	print(f'\nThe ISS is currently at - \n{lat_now}\n{long_now}\n')
+
+	print(f"At the coordinates ( {passes_data['request']['latitude']}, {passes_data['request']['longitude']} ) , The ISS will pass over {passes_data['request']['passes']} times. -")
+	for pass_ in passes:
+	    print(f'On {pass_[1]} for a duration of {pass_[0]} seconds')
+
+	print(f'\nCurrently {crew_data['number']} people are on the ISS - ')
+	for person in crew_data['people']:
+	    print(person['name'])
+
+
+if __name__ == '__main__':
+	display_data()
+
+    
